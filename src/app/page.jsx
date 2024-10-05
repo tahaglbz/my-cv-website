da5b39d4a5be48fa80d958945ac0215c4dd657b2
tahaglbz <tahaglbz1@gmail.com>
2024-10-06 00:51:57 +0300

    homepage update
    should be fixed image tag

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,6 +1,7 @@
 import { Button } from "@/components/ui/button";
 import { FiDownload } from "react-icons/fi";
 import Social from "@/components/Social";
+import Photo from "@/components/Photo";
 
 const Home = () => {
   return (
@@ -11,7 +12,7 @@ const Home = () => {
           <div className="text-center xl:text-left">
             <span className="text-xl">Software Engineer</span>
             <h1 className="h1">Hello I&apos;m <br />
-              <span>Taha Gülbaz</span></h1>
+              <span className="text-accent">Taha Gülbaz</span></h1>
             <p className="max-w-[500px] mb-9 text-white/80">I am a 3rd year software engineering student. I am working on areas such as mobile application development, backend development and data science. I develop projects for competitions such as Teknofest and TÜBİTAK. </p>
             {/* btn and socials */}
             <div className="flex flex-col xl:flex-row items-center gap-8">
@@ -19,11 +20,13 @@ const Home = () => {
                 <span>Download CV</span>
                 <FiDownload className="text-xl" />
               </Button>
-              <div className="mb-8 xl:mb-0"><Social/></div>
+              <div className="mb-8 xl:mb-0"><Social containerStyles="flex gap-6" iconStyles="w-9 h-9 border border-accent rounded-full flex justify-center items-center text-accent text-base hover:bg-accent hover:text-primary hover:transition-all duration-500"/></div>
             </div>
           </div>
           {/* photo */}
-          <div>photo</div>
+          <div>
+            <Photo />
+          </div>
 
         </div>
       </div> 
--- src/components/Social.jsx
+++ src/components/Social.jsx
@@ -1,9 +1,20 @@
 import Link from "next/link";
-import { FaGithub, FaLinkedinIn, FaInstagram} from 'react-icons/fa';
-const Social = () => {
+import { FaGithub, FaLinkedinIn, FaInstagram, FaDiscord } from 'react-icons/fa';
+
+const socials = [
+  { icon: <FaGithub />, path: '' },
+  { icon: <FaLinkedinIn />, path: '' },
+  { icon: <FaInstagram />, path: '' },
+  { icon: <FaDiscord />, path: '' },
+]
+const Social = ({containerStyles, iconStyles}) => {
   return (
-    <div>
-      Social
+    <div className = {containerStyles}>
+      {socials.map((item, index) => {
+        return <Link key={index} href={item.path} className={iconStyles}>
+        {item.icon}
+        </Link>
+      })}
     </div>
   )
 }
