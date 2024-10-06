ee07c61d68ec03dbb3af329c2d8f0a0911a96212
tahaglbz <tahaglbz1@gmail.com>
2024-10-07 02:27:24 +0300

    services page finished

--- src/app/services/page.jsx
+++ src/app/services/page.jsx
@@ -33,27 +33,39 @@ const services = [
 
 const Services = () => {
   return (
-    <section className="min-h-[80vh] flex flex-col justify-center py-12 xl:py-0">
-      <div className="container mx-auto">
-        <motion.div className="grid grid-cols-1 md:grid-cols-2 gap-[60px]" initial={{opacity:0}} animate={{opacity:1,transition:{delay:2.4, duration:0.4, ease:"easeIn"},}} >
-          {services.map((services, index) => {
-            return (
-              <div key={index} className="flex-1 flex flex-col justify-center gap-6 group">
-                <div className="w-full flex justify-between items-center">
-                  <div className=" text-5xl font-extrabold">{services.num}</div>
-                  {/* title */}
-                  <h2>{services.title}</h2>
-                  {/* descriotion */}
-                  <p>{services.description}</p>
-                  {/* border */}
-                  <div className="border-b border-white/20 w-full"></div>
-                </div>
+   <section className="min-h-[80vh] flex flex-col justify-center py-12 xl:py-0">
+  <div className="container mx-auto">
+    <motion.div
+      className="grid grid-cols-1 md:grid-cols-2 gap-[60px]"
+      initial={{ opacity: 0 }}
+      animate={{
+        opacity: 1,
+        transition: { delay: 2.4, duration: 0.4, ease: "easeIn" },
+      }}
+    >
+      {services.map((service, index) => {
+        return (
+          <div key={index} className="flex flex-col justify-center gap-6 group">
+            <div className="w-full flex justify-between items-center">
+              {/* Number */}
+              <div className="text-5xl font-extrabold text-outline text-transparent group-hover:text-outline-hover transition-all duration-500">
+                {service.num}
               </div>
-            )
-          })}
-        </motion.div>
-      </div>
-    </section>
+              
+            </div>
+            {/* Title */}
+            <h2 className="text-[42px] font-bold leading-none text-white  group-hover:text-accent transition-all duration-500">{service.title}</h2>
+            {/* Description */}
+            <p className="text-white/80">{service.description}</p>
+            {/* Border */}
+            <div className="border-b border-white/20 w-full"></div>
+          </div>
+        );
+      })}
+    </motion.div>
+  </div>
+</section>
+
   )
 }
 
