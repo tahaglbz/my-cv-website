d3d9ec642cb47c64c354f9840be3525ec79b5dea
tahaglbz <tahaglbz1@gmail.com>
2024-10-08 13:54:40 +0300

    contact page finished

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -1,10 +1,84 @@
-import React from 'react'
+"use client"
+import { Button } from "@/components/ui/button"
+import { Input } from "@/components/ui/input"
+import { Textarea } from "@/components/ui/textarea"
+import {
+  Select, SelectContent, SelectGroup, SelectItem, SelectLabel, SelectTrigger, SelectValue
+} from "@/components/ui/select"
+import { FaPhoneAlt, FaEnvelope } from 'react-icons/fa'
+import { motion } from "framer-motion"
+
+const info = [
+  {
+    icon: <FaPhoneAlt />,
+    title: "Email",
+    description: "tahaglbz1@gmail.com"
+  },
+  {
+    icon: <FaEnvelope />,
+    title: "Phone",
+    description: "(+90) 5313256038"
+  },
+]
 
 const Contact = () => {
   return (
-    <div>
-      Contact Page
-    </div>
+    <motion.section initial={{ opacity: 0 }} animate={{ opacity: 1, transition: { delay: 2.4, duration: 0.4, ease: "easeIn" } }}
+      className="py-6">
+      <div className="container mx-auto">
+        <div className="flex flex-col xl:flex-row gap-[30px] xl:gap-12"> {/* Adjusted gap */}
+          <div className="xl:w-1/2 order-2 xl:order-none"> {/* 50% width for larger screens */}
+            <form action="" className="flex flex-col gap-6 p-10 bg-[#27272c] rounded-xl">
+              <h3 className="text-4xl text-accent">Let&apos;s work together</h3>
+              <p className="text-white/60">
+                You can send your offers and requests
+              </p>
+              <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
+                <Input type="firstname" placeholder="Firstname" />
+                <Input type="lastname" placeholder="Lastname" />
+                <Input type="email" placeholder="Email address" />
+                <Input type="phone" placeholder="Phone number" />
+              </div>
+              <Select>
+                <SelectTrigger className="w-full">
+                  <SelectValue placeholder="Select a service"></SelectValue>
+                </SelectTrigger>
+                <SelectContent>
+                  <SelectGroup>
+                    <SelectLabel></SelectLabel>
+                    <SelectItem value="web">Web Development</SelectItem>
+                    <SelectItem value="mob">Mobile App Development</SelectItem>
+                    <SelectItem value="desk">Desktop App Development</SelectItem>
+                    <SelectItem value="db">Database Design</SelectItem>
+                    <SelectItem value="oth">Other</SelectItem>
+                  </SelectGroup>
+                </SelectContent>
+              </Select>
+              <Textarea className="h-[200px]" placeholder="Type your message here" />
+              <Button size="md" className="max-w-40">Send Message</Button>
+            </form>
+          </div>
+
+          <div className="xl:w-1/2 flex items-center xl:justify-end order-1 xl:order-none mb-8 xl:mb-0"> {/* 50% width */}
+            <ul className="flex flex-col gap-10">
+              {info.map((item, index) => {
+                return (
+                  <li key={index} className="flex items-center gap-6">
+                    <div className="w-[52px] xl:w-[72px] xl:h-[72px] bg-[#27272c] text-accent rounded-md flex items-center justify-center">
+                      <div className="text-[28px]">{item.icon}</div>
+                    </div>
+                    <div className="flex-1">
+                      <p className="text-white/60">{item.title}</p>
+                      <h3 className="text-xl">{item.description}</h3>
+                    </div>
+                  </li>
+                )
+              })}
+            </ul>
+          </div>
+        </div>
+      </div>
+    </motion.section>
   )
 }
 
