56492767f79a61dd155f0289c8fadd7bcb748de4
tahaglbz <tahaglbz1@gmail.com>
2024-10-09 02:48:54 +0300

    Update Photo.jsx

--- src/components/Photo.jsx
+++ src/components/Photo.jsx
@@ -6,31 +6,53 @@ import Image from "next/image";
 const Photo = () => {
   return (
     <div className="w-full h-full flex items-center justify-center relative pr-64">
-      <motion.div 
-        initial={{ opacity: 0 }} 
-        animate={{ opacity: 1, transition: { delay: 2.4, duration: 0.4, ease: "easeIn" } }}
+      <motion.div
+        initial={{ opacity: 0 }}
+        animate={{
+          opacity: 1,
+          transition: { delay: 2.4, duration: 0.4, ease: "easeIn" },
+        }}
         className="relative"
       >
         {/* image */}
-        <motion.div 
-          initial={{ opacity: 0 }} 
-          animate={{ opacity: 1, transition: { delay: 2, duration: 0.4, ease: "easeInOut" } }}  
-          className="w-[298px] h-[298px] xl:w-[498px] mix-blend-lighten absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2"
+        <motion.div
+          initial={{ opacity: 0 }}
+          animate={{
+            opacity: 1,
+            transition: { delay: 2, duration: 0.4, ease: "easeInOut" },
+          }}
+          className="w-[250px] h-[250px] xl:w-[400px] xl:h-[400px] absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2"
+          style={{
+            clipPath: "circle(50%)",
+            boxShadow: "0px 0px 15px rgba(0, 0, 0, 0.3)", // Gölgelendirme ekledik
+          }}
         >
-          <Image src="/assets/meavatarr.png" priority quality={100} fill alt="avatar" className="object-contain" />
+          <Image
+            src="/assets/meavatarr.png"
+            priority
+            quality={100}
+            fill
+            alt="avatar"
+            className="object-cover"
+          />
         </motion.div>
-        
+
         {/* circle */}
-        <motion.svg className="w-[300px] xl:w-[506px] h-[300px] xl:h-[506px] absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2" fill="transparent" viewBox="0 0 506 506" xmlns="http://www.w3.org/2000/svg">
-          <motion.circle 
-            cx="253" 
-            cy="253" 
-            r="250" 
-            stroke="#00ff99" 
-            strokeWidth="4" 
-            strokeLinecap="round" 
-            strokeLinejoin="round" 
-            initial={{ strokeDasharray: "24 10 0 0" }} 
+        <motion.svg
+          className="w-[300px] xl:w-[506px] h-[300px] xl:h-[506px] absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2"
+          fill="transparent"
+          viewBox="0 0 506 506"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <motion.circle
+            cx="253"
+            cy="253"
+            r="250"
+            stroke="#00ff99"
+            strokeWidth="4"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            initial={{ strokeDasharray: "24 10 0 0" }}
             animate={{
               strokeDasharray: ["15 120 25 25", "16 25 92 72", "4 250 22 22"],
               rotate: [120, 360],
@@ -38,13 +60,13 @@ const Photo = () => {
             transition={{
               duration: 20,
               repeat: Infinity,
-              repeatType: "reverse"
+              repeatType: "reverse",
             }}
           />
         </motion.svg>
       </motion.div>
     </div>
   );
-}
+};
 
 export default Photo;
