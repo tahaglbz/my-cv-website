75129300899d384ca60a1153d6c8b1fcd4b92692
tahaglbz <tahaglbz1@gmail.com>
2024-10-09 13:59:56 +0300

    ..

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -10,12 +10,12 @@ import { motion } from "framer-motion"
 
 const info = [
   {
-    icon: <FaPhoneAlt />,
+    icon: <FaEnvelope />,
     title: "Email",
     description: "tahaglbz1@gmail.com"
   },
   {
-    icon: <FaEnvelope />,
+    icon: <FaPhoneAlt />,
     title: "Phone",
     description: "(+90) 5313256038"
   },
@@ -26,8 +26,8 @@ const Contact = () => {
     <motion.section initial={{ opacity: 0 }} animate={{ opacity: 1, transition: { delay: 2.4, duration: 0.4, ease: "easeIn" } }}
       className="py-6">
       <div className="container mx-auto">
-        <div className="flex flex-col xl:flex-row gap-[30px] xl:gap-12"> {/* Adjusted gap */}
-          <div className="xl:w-1/2 order-2 xl:order-none"> {/* 50% width for larger screens */}
+        <div className="flex flex-col xl:flex-row gap-[30px] xl:gap-12"> 
+          <div className="xl:w-1/2 order-2 xl:order-none"> 
             <form action="" className="flex flex-col gap-6 p-10 bg-[#27272c] rounded-xl">
               <h3 className="text-4xl text-accent">Let&apos;s work together</h3>
               <p className="text-white/60">
