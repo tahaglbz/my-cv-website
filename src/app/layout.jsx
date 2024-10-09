a23c1a3bd6afdd46dc55fb8fb50153a60f2ccb19
tahaglbz <tahaglbz1@gmail.com>
2024-10-09 15:49:35 +0300

    .

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -19,6 +19,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        
+      </head>
       <body className={jetbrainsMono.variable}>
         <Header />
         <StairTransition></StairTransition>
--- src/components/MobileNav.jsx
+++ src/components/MobileNav.jsx
@@ -35,7 +35,7 @@ const MobileNav = () => {
                 <div className="mt-32 mb-40 text-center text-2xl">
                     <Link href="/">
                         <h1 className="text-4xl font-semibold">
-                            Taha <span className="text-accent">.</span>
+                            Thaa<span className="text-accent">.</span>
                         </h1>
                     </Link>
                 </div>
